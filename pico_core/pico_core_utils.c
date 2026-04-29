79db97b40f14ab8fea4c7b49f8a9f03619ce0b42
Azer Oueslati <oueslati.2085605@studenti.uniroma1.it>
2026-04-30 00:13:12 +0200

    alltoall v1

--- pico_core/pico_core_utils.c
+++ pico_core/pico_core_utils.c
@@ -163,7 +163,7 @@ static inline alltoall_func_ptr get_alltoall_function(const char *algorithm) {
 #ifndef PICO_NCCL
   CHECK_STR(algorithm, "bine_over", alltoall_bine);
   CHECK_STR(algorithm, "pairwise_ompi_over", alltoall_pairwise_ompi);
-
+  CHECK_STR(algorithm, "bine_dh_over", alltoall_bine_DH);
   PICO_CORE_DEBUG_PRINT_STR("MPI_Alltoall");
   return alltoall_wrapper;
 #else
